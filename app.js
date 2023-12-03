652f6e259e84ca6abe21f2d4cecaf84f67fe2f39
ahmad90383 <138042529+ahmad90383@users.noreply.github.com>
2023-12-03 09:38:43 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 5000;
-// const colors = require("colors");
-// const dotenv = require("dotenv");
-// const path = require("path");
-// const cors = require("cors");
-
-// dotenv.config(); // Move dotenv configuration to the top
-// dotenv.config({ path: "./config.env" });
-
-// require("./dbConnection");
-
-// app.use(express.json());
-// app.use(cors());
-// // Enable CORS middleware
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
-
-// app.use("/api", require("./router/fetchData"));
-// app.use(require("./router/userRoute"));
-// app.use(require("./router/postOrder"));
-
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}`.bgGreen);
-// });
-
-// for hosting
-// ************************************************************ */
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -51,18 +11,17 @@ const connectToDatabase = require("./dbConnection");
 
 app.use(express.json());
 
+// API routes
 app.use("/api", require("./router/fetchData"));
 app.use(require("./router/userRoute"));
 app.use(require("./router/postOrder"));
 
 // Serve static files
-// app.use(express.static(path.join(__dirname, "./super-mart/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./super-mart/build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "./super-mart/build")));
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+// Route handling for all routes not handled by API routes
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./super-mart/build", "index.html"));
 });
 
 // Error handling middleware
@@ -76,12 +35,8 @@ app.use((req, res, next) => {
   res.status(404).json({ message: "Route not found" });
 });
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "./client/build")));
-
-// Handle all remaining routes by serving the index.html file
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
 app.listen(port, () => {
